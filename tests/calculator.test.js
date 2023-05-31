0f16b4e35dfdda600d2ba555fdd6f120347b771a
Roxana Falco <roxana.falco20@gmail.com>
2023-05-31 01:13:40 -0300

    Agrego calculator.test.js incompleta para el ejercicio

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,40 +3,56 @@ import Calculator from '../src/calculator';
 
 /* Jest Cheatsheet https://github.com/sapegin/jest-cheat-sheet */
 
-let calculator;
+const calculator = new Calculator();
 
 describe('Calculator', () => {
-  beforeEach(() => {
-    calculator = new Calculator();
-  });
+  describe('add', () => {
+    test('should return the sum of two positive numbers', () => {
+      expect(calculator.add(2, 3)).toBe(5);
+    });
 
-  describe('add()', () => {
-    test('', () => {
+    test('should return the sum of a positive and a negative number', () => {
+      expect(calculator.add(2, -3)).toBe(-1);
+    });
 
+    test('should return the sum of two negative numbers', () => {
+      /* TODO*/
     });
   });
 
-  describe('subtract()', () => {
-    test('', () => {
+  describe('subtract', () => {
+    test('should return the difference between two positive numbers', () => {
+      expect(calculator.subtract(3, 2)).toEqual(1);
+    });
 
+    test('should return the difference between a positive and a negative number', () => {
+      /* TODO*/
+    });
+
+    test('should return the difference between two negative numbers', () => {
+      /* TODO*/
     });
   });
 
-  describe('multiply()', () => {
-    test('', () => {
+  describe('multiply', () => {
+    test('should return the product of two positive numbers', () => {
+      /* TODO*/
+    });
 
+    test('should return the product of a positive and a negative number', () => {
+      /* TODO*/
+    });
+
+    test('should return the product of two negative numbers', () => {
+      /* TODO*/
     });
   });
 
-  describe('divide()', () => {
-    test('', () => {
-
-    });
+  describe('divide', () => {
+    /* TODO*/
   });
 
   describe('squareRoot(num)', () => {
-    test('', () => {
-
-    });
+    /* TODO*/
   });
 });
